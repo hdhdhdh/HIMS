fa8756a2ecfbc802b213e97fcd439d24e2f69cd3
Fangyichuan <1197468079@qq.com>
2019-07-01 20:03:39 +0800

    方异川1

--- src/main/java/com/whut/dao/IDoctorDao.java
+++ src/main/java/com/whut/dao/IDoctorDao.java
@@ -1,4 +1,13 @@
 package com.whut.dao;
 
+import com.whut.bean.Doctor;
+
+import java.util.List;
+
 public interface IDoctorDao {
+    List<Doctor> findDoctor();
+    void deleteDoc(int d_id);
+    void addDoc(Doctor doctor);
+    void updateDoc(Doctor doctor);
+    Doctor findId(int d_id);
 }
