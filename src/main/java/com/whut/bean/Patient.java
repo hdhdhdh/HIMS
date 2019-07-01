c91f14877d98dab965f798cebcef4c616411226a
Fangyichuan <1197468079@qq.com>
2019-07-01 16:54:21 +0800

    方异川

--- src/main/java/com/whut/bean/Patient.java
+++ src/main/java/com/whut/bean/Patient.java
@@ -1,5 +1,64 @@
 package com.whut.bean;
+
+import java.util.Date;
+
 //wirte by sunfahu
-public class Patient
-{
+public class Patient {
+    private char p_id;
+    private String p_name;
+    private int p_gender;
+    private Date p_birthday;
+    private String p_password;
+
+    public char getP_id() {
+        return p_id;
+    }
+
+    public void setP_id(char p_id) {
+        this.p_id = p_id;
+    }
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public int getP_gender() {
+        return p_gender;
+    }
+
+    public void setP_gender(int p_gender) {
+        this.p_gender = p_gender;
+    }
+
+    public Date getP_birthday() {
+        return p_birthday;
+    }
+
+    public void setP_birthday(Date p_birthday) {
+        this.p_birthday = p_birthday;
+    }
+
+    public String getP_password() {
+        return p_password;
+    }
+
+    public void setP_password(String p_password) {
+        this.p_password = p_password;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "p_id=" + p_id +
+                ", p_name='" + p_name + '\'' +
+                ", p_gender=" + p_gender +
+                ", p_birthday=" + p_birthday +
+                ", p_password='" + p_password + '\'' +
+                '}';
+    }
 }
+
--- src/main/java/com/whut/bean/Type.java
+++ src/main/java/com/whut/bean/Type.java
@@ -1,5 +1,30 @@
 package com.whut.bean;
 //code by sunfahu
-public class Type
-{
+public class Type {
+    private char t_id;
+    private String t_name;
+
+    public char getT_id() {
+        return t_id;
+    }
+
+    public void setT_id(char t_id) {
+        this.t_id = t_id;
+    }
+
+    public String getT_name() {
+        return t_name;
+    }
+
+    public void setT_name(String t_name) {
+        this.t_name = t_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "t_id=" + t_id +
+                ", t_name='" + t_name + '\'' +
+                '}';
+    }
 }
