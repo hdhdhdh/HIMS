514568506f6b425a2ccf800a946f1b5512b87ddf
Fangyichuan <1197468079@qq.com>
2019-07-01 18:43:14 +0800

    方异川

--- src/main/java/com/whut/dao/ICase.java
+++ src/main/java/com/whut/dao/ICase.java
@@ -1,4 +1,9 @@
 package com.whut.dao;
 
+<<<<<<< Updated upstream:src/main/java/com/whut/dao/ICase.java
 public interface ICase {
+=======
+public class Dao {
+
+>>>>>>> Stashed changes:src/main/java/com/whut/dao/Dao.java
 }
