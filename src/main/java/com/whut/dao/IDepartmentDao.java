2bb318b37f139d5b990a806f24c82301c215ae58
xinmiaoya <1172774037@qq.com>
2019-07-02 14:24:09 +0800

    by liu  7.2.4

--- src/main/java/com/whut/dao/IDepartmentDao.java
+++ src/main/java/com/whut/dao/IDepartmentDao.java
@@ -1,5 +1,14 @@
 package com.whut.dao;
 
+import com.whut.bean.Department;
+
+import java.util.List;
+
 public interface IDepartmentDao
 {
+    List<Department> findDepartment();
+    void addDepartment();
+    void deleteDepartment();
+    void updateDepartment();
+    Department findDepartmentById(String id);
 }
