50ff95aa5b46913d38ee78d94c19a101ae9e75c0
SFH <sunfahu@outlook.com>
2019-07-02 17:09:36 +0800

    sunfahu 7.2.17.09

--- src/main/java/com/whut/dao/IDoctorDao.java
+++ src/main/java/com/whut/dao/IDoctorDao.java
@@ -1,16 +1,16 @@
 package com.whut.dao;
 
 import com.whut.bean.Doctor;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface IDoctorDao {
-    List<Doctor> findDoctor();//导出该医生信息
-    List<Doctor> findAllDoctor();//导出所有医生信息
+    List<Doctor> getAllDoctor();//导出所有医生信息
     void deleteDoctor(String d_id);//删除医生信息
     void addDoctor(Doctor doctor);//增加医生
-    void updateDoctor(Doctor doctor);//医生更新医生信息
-    void Mea_updateDoctor(Doctor doctor,String d_id);//管理员更新医生信息
-    Doctor findDoctorById(String d_id);//查找医生的id
+    void updateDoctorWithoutId(Doctor doctor);//医生更新医生信息
+    void updateDoctorWithId(@Param("newDoctor") Doctor newDoctor,  @Param("target") String target);//管理员更新医生信息
+    Doctor getDoctorById(String d_id);//查找医生的id
 
 }
