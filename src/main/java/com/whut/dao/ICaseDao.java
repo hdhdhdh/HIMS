2c40f737a76672c1733d9fcd74dcb48027b328b6
Fangyichuan <1197468079@qq.com>
2019-07-03 19:58:53 +0800

    方异川6

--- src/main/java/com/whut/dao/ICaseDao.java
+++ src/main/java/com/whut/dao/ICaseDao.java
@@ -6,10 +6,10 @@ import com.whut.bean.Case;
 import java.util.List;
 
 public interface ICaseDao {
-    public List<Case> getAllCase();
-    public Case getCaseById(int c_id);
-    public void addCase(Case type);
-    public void updateCase(Case type);
-    public void deleteCase(int c_id);
+    public List<Case> getAllCase();//导出所有病例
+    public Case getCaseById(int c_id);//通过病例id得到病例
+    public void addCase(Case icase);//增加病例
+    public void updateCase(Case icase);//更改病例
+    public void deleteCase(int c_id);//删除病例
     
 }
--- src/main/java/com/whut/service/imp/CaseService.java
+++ src/main/java/com/whut/service/imp/CaseService.java
@@ -1,13 +1,18 @@
 package com.whut.service.imp;
 
 import com.whut.bean.Case;
+import com.whut.dao.ICaseDao;
 import com.whut.service.ICaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CaseService implements ICaseService {
+
+    @Autowired
+    public ICaseDao iCaseDao;
     @Override
     public List<Case> getAllCase() {
         return null;
@@ -19,17 +24,34 @@ public class CaseService implements ICaseService {
     }
 
     @Override
-    public boolean addCase(Case ncase) {
+    public boolean addCase(Case icase) {
+        try
+        {
+            iCaseDao.addCase(icase);
+            return true;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateCase(Case icase) {
         return false;
     }
 
     @Override
-    public boolean updateCase(Case ncase) {
-        return false;
-    }
+    public boolean deleteCase(int c_id) {
 
-    @Override
-    public boolean deleteCase(String c_id) {
-        return false;
+        try
+        {
+            iCaseDao.deleteCase(c_id);
+            return true;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
